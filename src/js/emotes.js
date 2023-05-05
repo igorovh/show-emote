92bff523fbd4c6a5c5569d3195f486b376a36851
animekkk <animekgaming@gmail.com>
2023-05-06 01:12:34 +0200

    Adding support for global emotes

--- src/js/emotes.js
+++ src/js/emotes.js
@@ -5,17 +5,32 @@ window.addEventListener('DOMContentLoaded', () => {
 	loadEmotes();
 });
 
-const loadEmotes = () => {
-	fetch(`https://emotes.adamcy.pl/v1/channel/${config.CHANNEL}/emotes/all`)
-		.then((response) => response.json())
-		.then((json) => {
-			emotes = json;
+const loadEmotes = async () => {
+	emotes = [];
+	Promise.all([
+		new Promise((resolve, reject) => {
+			fetch(`https://emotes.adamcy.pl/v1/channel/${config.CHANNEL}/emotes/all`)
+				.then((response) => response.json())
+				.then((json) => {
+					resolve(json);
+				});
+		}),
+		new Promise((resolve, reject) => {
+			fetch(`https://emotes.adamcy.pl/v1/global/emotes/all`)
+				.then((response) => response.json())
+				.then((json) => {
+					resolve(json);
+				});
+		}),
+	]).then((values) => {
+		emotes.push(...values[0]);
+		emotes.push(...values[1]);
 
-			if (reload) displayImage('https://public.igor.ovh/reloaded.gif');
-			logMessage(`Successfully ${reload ? 're' : ''}loaded ${emotes.length} emotes on channel ${config.CHANNEL}.`);
+		if (reload) displayImage('https://public.igor.ovh/reloaded.gif');
+		logMessage(`Successfully ${reload ? 're' : ''}loaded ${emotes.length} emotes on channel ${config.CHANNEL}.`);
 
-			reload = true;
-		});
+		reload = true;
+	});
 };
 
 const getEmoteByCode = (code) => {

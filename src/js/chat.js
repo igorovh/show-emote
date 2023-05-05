c396b3325bab17e78536b6d466f0cbbebf2f3066
animekkk <animekgaming@gmail.com>
2023-05-06 01:00:37 +0200

    Fixes again

--- src/js/chat.js
+++ src/js/chat.js
@@ -1,88 +1,92 @@
-const client = new tmi.Client({
-	channels: [config.CHANNEL],
-});
+window.addEventListener('config', () => {
+	console.info(`Creating chat client for ${config.CHANNEL}.`);
 
-client.connect();
+	const client = new tmi.Client({
+		channels: [config.CHANNEL],
+	});
 
-const COOLDOWNS = {
-	showemote: 0,
-};
+	client.connect();
 
-const commands = [
-	{
-		names: ['showemote'],
-		moderator: false,
-		executor: (data, args, ignoreCooldown = false) => {
-			if (!ignoreCooldown) {
-				if (COOLDOWNS['showemote'] >= Date.now()) return;
-				COOLDOWNS['showemote'] = Date.now() + config.SHOW_EMOTE_COOLDOWN * 1000;
-			}
-
-			if (args.length < 1) return;
-			const code = args[0];
-			const emote = getEmoteByCode(code);
-			if (!emote) return;
-			displayImage(emote.urls[emote.urls.length - 1].url);
-			console.info('Showing emote with code', code, `and adding cooldown for ${config.SHOW_EMOTE_COOLDOWN} secs`);
-		},
-	},
-	{
-		names: ['forceshowemote', 'fshowemote'],
-		moderator: true,
-		executor: (data, args) =>
-			commands.find((command) => command.names.includes('showemote')).executor(data, args, true),
-	},
-	{
-		names: ['reloademotes'],
-		moderator: true,
-		executor: (data, args) => loadEmotes(),
-	},
-	{
-		names: ['showaha'],
-		moderator: true,
-		executor: (data, args) => {
-			const ahaEmotes = emotes.filter((emote) => emote.code.startsWith('aha'));
-			if (ahaEmotes.length < 1) return;
-			ahaEmotes.forEach((emote) =>
-				setTimeout(() => displayImage(emote.urls[emote.urls.length - 1].url), randomRange(1, 50) * 100)
-			);
-		},
-	},
-	{
-		names: ['showbooba'],
-		moderator: true,
-		executor: (data, args) => {
-			const boobaCodes = config.BOOBA_CODES.split(',');
-			const boobaEmotes = [];
-			boobaCodes.forEach((code) => {
-				const emote = getEmoteByCode(code);
-				if (emote) boobaEmotes.push(emote);
-			});
-			boobaEmotes.forEach((emote) =>
-				setTimeout(() => displayImage(emote.urls[emote.urls.length - 1].url), randomRange(1, 50) * 100)
-			);
-		},
-	},
-];
-
-client.on('message', (channel, tags, message, self) => {
-	if (self || !message.startsWith('!')) return;
-
-	const args = message.slice(1).split(' ');
-	const commandName = args.shift().toLowerCase();
-
-	const data = {
-		username: tags['display-name'],
-		userId: tags['user-id'],
-		moderator: tags.mod || tags.badges?.broadcaster !== undefined,
-		channel,
-		roomId: tags['room-id'],
+	const COOLDOWNS = {
+		showemote: 0,
 	};
 
-	const command = commands.find((command) => command.names.includes(commandName));
-	if (!command) return;
+	const commands = [
+		{
+			names: ['showemote'],
+			moderator: false,
+			executor: (data, args, ignoreCooldown = false) => {
+				if (!ignoreCooldown) {
+					if (COOLDOWNS['showemote'] >= Date.now()) return;
+					COOLDOWNS['showemote'] = Date.now() + config.SHOW_EMOTE_COOLDOWN * 1000;
+				}
 
-	if (command.moderator && !data.moderator && !config.ALLOW_LIST.includes(data.username)) return;
+				if (args.length < 1) return;
+				const code = args[0];
+				const emote = getEmoteByCode(code);
+				if (!emote) return;
+				displayImage(emote.urls[emote.urls.length - 1].url);
+				console.info('Showing emote with code', code, `and adding cooldown for ${config.SHOW_EMOTE_COOLDOWN} secs`);
+			},
+		},
+		{
+			names: ['forceshowemote', 'fshowemote'],
+			moderator: true,
+			executor: (data, args) =>
+				commands.find((command) => command.names.includes('showemote')).executor(data, args, true),
+		},
+		{
+			names: ['reloademotes'],
+			moderator: true,
+			executor: (data, args) => loadEmotes(),
+		},
+		{
+			names: ['showaha'],
+			moderator: true,
+			executor: (data, args) => {
+				const ahaEmotes = emotes.filter((emote) => emote.code.startsWith('aha'));
+				if (ahaEmotes.length < 1) return;
+				ahaEmotes.forEach((emote) =>
+					setTimeout(() => displayImage(emote.urls[emote.urls.length - 1].url), randomRange(1, 50) * 100)
+				);
+			},
+		},
+		{
+			names: ['showbooba'],
+			moderator: true,
+			executor: (data, args) => {
+				const boobaCodes = config.BOOBA_CODES.split(',');
+				const boobaEmotes = [];
+				boobaCodes.forEach((code) => {
+					const emote = getEmoteByCode(code);
+					if (emote) boobaEmotes.push(emote);
+				});
+				boobaEmotes.forEach((emote) =>
+					setTimeout(() => displayImage(emote.urls[emote.urls.length - 1].url), randomRange(1, 50) * 100)
+				);
+			},
+		},
+	];
 
-	command.executor(data, args);
+	client.on('message', (channel, tags, message, self) => {
+		if (self || !message.startsWith('!')) return;
+
+		const args = message.slice(1).split(' ');
+		const commandName = args.shift().toLowerCase();
+
+		const data = {
+			username: tags['display-name'],
+			userId: tags['user-id'],
+			moderator: tags.mod || tags.badges?.broadcaster !== undefined,
+			channel,
+			roomId: tags['room-id'],
+		};
+
+		const command = commands.find((command) => command.names.includes(commandName));
+		if (!command) return;
+
+		if (command.moderator && !data.moderator && !config.ALLOW_LIST.includes(data.username)) return;
+
+		command.executor(data, args);
+	});
 });
--- src/js/config.js
+++ src/js/config.js
@@ -2,7 +2,7 @@ const config = {
 	CHANNEL: 'igor_ovh',
 	SHOW_EMOTE_TIME: 4,
 	SHOW_EMOTE_COOLDOWN: 60,
-	ALLOW_LIST: ['igor_ovh'],
+	ALLOW_LIST: [],
 	BOOBA_CODES:
 		'POGGIES,tutorial,ematerasu,tadimason,THAT,AnySubGiftersInChat,please,pillow,hiheyhello,peepoSit,piksele,wideDvaAss,NewOutfit,CATBOOBS,GoodNight,mods,THIS,HUGS,mods,pjpj,rawr,HeyCutie,imready,GoodMusic,tssk,raveGirl,Lounge,YouAreVeryInteresting,BOOTY,npasThighs,tenderly,dollKisses,Kissy,dollBunny,JuwnaThighs,nonono,hannahNo,hannahYes,igotsomethingforyou,THOSE,LOOK,sussy,booty,roseDance,areyouaboy,NightRoutine,WhatYouLookingAt,Dubai,MyReactionToThatInformation,pantsu,BOOBAS',
 };
@@ -14,6 +14,8 @@ window.addEventListener('DOMContentLoaded', () => {
 	if (params.has('channel')) config.CHANNEL = params.get('channel');
 	if (params.has('showEmoteTime')) config.SHOW_EMOTE_TIME = parseInt(params.get('showEmoteTime'));
 	if (params.has('showEmoteCooldown')) config.SHOW_EMOTE_COOLDOWN = parseInt(params.get('showEmoteCooldown'));
-	if (params.has('allowList')) config.ALLOW_LIST = params.get('showEmoteCooldown').split(',');
+	if (params.has('allowList')) config.ALLOW_LIST = params.get('allowList').split(',');
 	if (params.has('boobaCodes')) config.BOOBA_CODES = params.get('boobaCodes');
+
+	window.dispatchEvent(new Event('config'));
 });
